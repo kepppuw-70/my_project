f06023b1bafeeab6b96431a8b836b7764c7658a6
kepppuw_70 <tsgdnepro@3g.ua>
2020-09-01 23:17:49 +0300

    options clubs

--- club_sitting.php
+++ club_sitting.php
@@ -42,6 +42,9 @@
 .pozishn_menu{
   text-align: center;
 }
+.but1{
+  text-align: center;
+}
     </style>
   </head>
   <body>
@@ -84,10 +87,35 @@ echo '<h2 class="zaglav">Настройка параметров клуба</div
 
 ?> 
 
+
+<br><br><br>
+
+    <h4 style="text-align: center;">Активированные группы</h4>
+<div>
+    <table class="pozisn_cente" border="1">
+      <tr>
+        <th  width="40" class="cell">№</th>
+        <th class="cell">Клуб/Группа</th>
+        <th class="cell">Дни занятий</th>
+        <th class="cell">Цена тр./ф.м.</th>
+      </tr>
+      <?php
+        echo '<tr>
+                 <td>'.'1'.'</td>
+                 <td></td>
+                 <td></td>
+                 <td></td>
+            </tr>';
+      ?>
+      
+    </table>
+</div>   
+
+
 <br><br>
 
 <?php
-
+/*
 
 $pai = 380;  // сумма последнего платежа
 $cast_van_day = 41.5; // стоимость одного дня занятий
@@ -163,9 +191,14 @@ unset($arr_tr_day[$pai_for_day]);
 <?php  
 show_arr($arr_tr_day, 'name');
 echo 'заплпнированная дата нового взноса - '.$data_next_pai.'/'.$munz.'/'.$yar;
+
+echo '</div>';
+
+
+*/
 ?>
 
-</div>
+
 
 
 
@@ -182,7 +215,7 @@ echo 'заплпнированная дата нового взноса - '.$dat
   <legend>
     <b>Активация места тренеровок</b>
   </legend>
-
+<form action="club_sitting.php" method="POST">
 <?php
  
  if ($_SESSION['bd'] == 'EO') {
@@ -192,11 +225,9 @@ echo 'заплпнированная дата нового взноса - '.$dat
  echo '<div class="club"><label for="club">Клуб</label>
    <select name="club">
      <option id="club" value="School54">СШ54</option>
-     <option id="club" value="School99">СШ99</option>
-  </select></div>';
+   </select></div>';
  echo '<div class="grup"><label for="grupp">Группа</label>
    <select name="grupp">
-     <option id="grupp" value="g0">Самоподготовка</option>
      <option id="grupp" value="g1">Группа 1</option>
      <option id="grupp" value="g2">Группа 2</option>
      <option id="grupp" value="g3">Группа 3</option>
@@ -205,6 +236,7 @@ echo 'заплпнированная дата нового взноса - '.$dat
      <option id="grupp" value="g6">Группа 6</option>
      <option id="grupp" value="g7">Группа 7</option>
      <option id="grupp" value="g8">Группа 8</option>
+     <option id="grupp" value="g9">Группа 9</option>
    </select></div>';
    
 }
@@ -220,7 +252,6 @@ echo 'заплпнированная дата нового взноса - '.$dat
   </select></div>';
  echo '<div class="grup"><label for="grupp">Группа</label>
    <select name="grupp">
-     <option id="grupp" value="g0">Самоподготовка</option>
      <option id="grupp" value="g1">Группа 1</option>
      <option id="grupp" value="g2">Группа 2</option>
      <option id="grupp" value="g3">Группа 3</option>
@@ -240,10 +271,11 @@ echo 'заплпнированная дата нового взноса - '.$dat
  echo '<div class="club"><label for="club">Клуб</label>
    <select name="club">
      <option id="club" value="School55">СШ55</option>
+     <option id="club" value="School99">СШ99</option>
    </select></div>';
  echo '<div class="grup"><label for="grupp">Группа</label>
    <select name="grupp">
-     <option id="grupp" value="g0">Самоподготовка</option>
+     
      <option id="grupp" value="g1">Группа 1</option>
      <option id="grupp" value="g2">Группа 2</option>
      <option id="grupp" value="g3">Группа 3</option>
@@ -262,13 +294,14 @@ echo 'заплпнированная дата нового взноса - '.$dat
  echo '<div>г.Днепр,</div>';
  echo '<div class="club"><label for="club">Клуб</label>
    <select name="club">
+     <option id="club" value="School1">СШ1</option>
      <option id="club" value="School133">СШ133</option>
      <option id="club" value="School00">Клуб</option>
      
   </select></div>';
  echo '<div class="grup"><label for="grupp">Группа</label>
    <select name="grupp">
-     <option id="grupp" value="g0">Самоподготовка</option>
+    
      <option id="grupp" value="g1">Группа 1</option>
      <option id="grupp" value="g2">Группа 2</option>
      <option id="grupp" value="g3">Группа 3</option>
@@ -287,7 +320,7 @@ echo 'заплпнированная дата нового взноса - '.$dat
  echo '<div>г.Днепр,</div>';
  echo '<div class="club"><label for="club">Клуб</label>
    <select name="club">
-     <option id="club" value="School1">СШ1</option>
+     
      <option id="club" value="School119">СШ119</option>
      <option id="club" value="School119">СШ101</option>
      <option id="club" value="School137">СШ137</option>
@@ -295,7 +328,7 @@ echo 'заплпнированная дата нового взноса - '.$dat
   </select></div>';
  echo '<div class="grup"><label for="grupp">Группа</label>
    <select name="grupp">
-     <option id="grupp" value="g0">Самоподготовка</option>
+     
      <option id="grupp" value="g1">Группа 1</option>
      <option id="grupp" value="g2">Группа 2</option>
      <option id="grupp" value="g3">Группа 3</option>
@@ -313,106 +346,65 @@ echo 'заплпнированная дата нового взноса - '.$dat
 
 ?>
 
-<br><br>
-<legend>
+
+   <p>
+    <input id="mon" type="checkbox" name="mon" value="1">
+    <label for="mon">Понедельник</label>
+    <input id="tues" type="checkbox" name="tues" value="2">
+    <label for="tues">Вторник</label>
+    <input id="wed" type="checkbox" name="wed" value="3">
+    <label for="wed">Среда</label>
+    <input id="thurs" type="checkbox" name="thurs" value="4">
+    <label for="thurs">Четверг</label>
+    <input id="fri" type="checkbox" name="fri" value="5">
+    <label for="fri">Пятница</label>
+    <input id="sat" type="checkbox" name="sat" value="6">
+    <label for="sat">Суббота</label>
+    <input id="sun" type="checkbox" name="sun" value="0">
+    <label for="sun">Воскресенье</label>
+   </p>
+
+
+<!--<legend>
     <b>Открытые группы</b>
   </legend>
-
+-->
  </fieldset>
-
-
-
-
- <br><br><br><br><br><br><br><br><br><br>
-
-
-
-
-
-
-
-
-
-
-
-
-
-<?php
-
-
-$pai = 380;  // сумма последнего платежа
-$cast_van_day = 41.5; // стоимость одного дня занятий
-$pai_for_day = Floor($pai/$cast_van_day); //число оплаченных дней
-$day_last_of_pai = '30/12/2020'; // дата последнего платежа
-$pis_day_last_of_pai = explode("/", $day_last_of_pai);
-$day = $pis_day_last_of_pai[0]; //день последнего платежа
-$munz = $pis_day_last_of_pai[1]; //месяц последнего платнжа
-$yar = $pis_day_last_of_pai[2]; //год последнего платежа
-// Дни треннеровок группы: 0 - воскресеньн и т.д.
-$tr1 = 1; 
-$tr2 = 3;
-$tr3 = 5;
-//
-
-//echo $pai_for_day;
-$days_in_munz = date("t", mktime(0, 0, 0, $munz, $day, $yar));
-//echo $days_in_munz.'<br>';
-$k = 0;
-for ($i=$day; $i <= $days_in_munz; $i++) {
-   if ($k < $pai_for_day + 1) {
-      if (date("w", mktime(0, 0, 0, $munz, $i, $yar)) == $tr1) {
-         $arr_tr_day[$k] = $i;
-         $k++;    
-      }
-      if (date("w", mktime(0, 0, 0, $munz, $i, $yar)) == $tr2) {
-         $arr_tr_day[$k] = $i;
-         $k++;    
-      }
-      if (date("w", mktime(0, 0, 0, $munz, $i, $yar)) == $tr3) {
-         $arr_tr_day[$k] = $i;
-         $k++;    
-      }
-    }
-}
-$num = count($arr_tr_day);
-if ($num < $pai_for_day + 1) {
-  if ($munz == 12) {
-    $munz = 1;
-    $yar = $yar + 1;
-  }
-  else{
-    $munz = $munz+1;
-  }
-  $days_in_munz = date("t", mktime(0, 0, 0, $munz  , $day, $yar));
-  $k = $num;
-  for ($i=0; $i < $days_in_munz; $i++) {
-    if ($k < $pai_for_day + 1) {
-      if (date("w", mktime(0, 0, 0, $munz  , $i+1, $yar)) == $tr1) {
-         $arr_tr_day[$k] = $i+1;
-         $k++;    
-      }
-      if (date("w", mktime(0, 0, 0, $munz  , $i+1, $yar)) == $tr2) {
-         $arr_tr_day[$k] = $i+1;
-         $k++;    
-      }
-      if (date("w", mktime(0, 0, 0, $munz  , $i+1, $yar)) == $tr3) {
-         $arr_tr_day[$k] = $i+1;
-         $k++;    
-      }
-    }
-
-  }
+<fieldset style="text-align: center;">
   
-}
+    <h5>Взнос за одну тренировку</h5>
+  
 
-$data_next_pai = $arr_tr_day[$pai_for_day];
-unset($arr_tr_day[$pai_for_day]);
-show_arr($arr_tr_day, 'name');
-echo 'заплпнированная дата нового взноса - '.$data_next_pai.'/'.$munz.'/'.$yar;
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control-born" size="1" placeholder="41.5" required name="pay_1_tren">
+    </div>
+    
+  </div>
 
-?>
+</fieldset>
+<br>
 
+<fieldset style="text-align: center;">
+  
+    <h5>Число не возвращаемых занятий</h5>
+  
 
+  <div class="row">
+    <div class="col">
+      <input type="text" class="form-control-born" size="1" placeholder="5" required name="free_trenings">
+    </div>
+    
+  </div>
+
+</fieldset>
+<br>
+
+<div class="but1">
+  <button type="submit" >Активировать</button>
+</div>
+
+</form>
 
 </div>
 <br><br><br>
